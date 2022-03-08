e25f055af2159dea3ebb8ca4a8e7ecca36baea98
Sushant Yerawar <sushantdyerawar2002@gmail.com>
2022-03-08 23:17:30 +0530

    first js commit

--- wordibble.js
+++ wordibble.js
@@ -1,1 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    let availableSpace = 1;
+
+    function getCurrentWordArr() {
+        const numberOfGuessedWords = guessedWords.length;
+        return guessedWords[numberOfGuessedWords - 1];
+    }
+
+    function handleDeleteLetter() {
+        const currentWordArr = getCurrentWordArr();
+        const removedLetter = currentWordArr.pop();
+
+        guessedWords[guessedWords.length - 1] = currentWordArr;
+
+        const lastLetterEl = document.getElementById(String(availableSpace - 1));
+
+        lastLetterEl.textContent = "";
+        availableSpace = availableSpace - 1;
+    }
+});
