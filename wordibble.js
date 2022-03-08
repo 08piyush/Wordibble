c5e0abfa2c202d30d7b9ea2aba966ef91ccab872
piyushhh <pancholipiyush08@gmail.com>
2022-03-08 23:35:27 +0530

    .js file modified 1

--- wordibble.js
+++ wordibble.js
@@ -19,3 +19,31 @@ document.addEventListener("DOMContentLoaded", () => {
         availableSpace = availableSpace - 1;
     }
 });
+
+function createSquares() {
+    const gameBoard = document.getElementById("board");
+
+    for (let index = 0; index < 30; index++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        square.classList.add("animate__animated");
+        square.setAttribute("id", index + 1);
+        gameBoard.appendChild(square);
+    }
+}
+
+function handleSubmitWord() {
+    const currentWordArr = getCurrentWordArr();
+    if (currentWordArr.length !== 5) {
+        window.alert("Word must be 5 letters");
+    }
+
+    const currentWord = currentWordArr.join("");
+
+    fetch(`https://wordsapiv1.p.rapidapi.com/words/${currentWord}`, {
+        method: "GET",
+        headers: {
+            "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+            "x-rapidapi-key": "61c5e3986dmsh20c1bee95c2230dp18d1efjsn4668bbcfc1b3",
+        },
+    })
